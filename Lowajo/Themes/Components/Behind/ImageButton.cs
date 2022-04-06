fc8672704065b10d43aea392d506c3f2aaa30758
helpandplay <helpandplay.kr@gmail.com>
2022-04-06 19:55:26 +0900

    FIx ImageButton structure
    - seperate xaml, cs

--- Lowajo/Themes/Components/Behind/ImageButton.cs
+++ Lowajo/Themes/Components/Behind/ImageButton.cs
@@ -2,16 +2,13 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace Lowajo.Themes.Components
+namespace Lowajo.Themes.Components.Behind
 {
-    /// <summary>
-    /// ImageButton.xaml에 대한 상호 작용 논리
-    /// </summary>
-    public partial class ImageButton : Button
+    public class ImageButton : Button
     {
         private readonly static DependencyProperty SourceProperty =
             DependencyProperty.Register(nameof(Source), typeof(string), typeof(ImageButton),
-                new PropertyMetadata(string.Empty));
+        new PropertyMetadata(string.Empty));
         private readonly static DependencyProperty ClickCommandProperty =
             DependencyProperty.Register(nameof(ClickCommand), typeof(ICommand), typeof(ImageButton),
                 new PropertyMetadata(null));
@@ -19,11 +16,6 @@ namespace Lowajo.Themes.Components
         static ImageButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton), new FrameworkPropertyMetadata(typeof(ImageButton)));
-        }
-
-        public ImageButton()
-        {
-            InitializeComponent();
         }
 
         public string Source
